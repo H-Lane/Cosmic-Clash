ac4404f5426e2d3d080e2b0aa644443a6f5a613c
B. Martin <imbrittanyalexis@gmail.com>
2024-08-07 00:26:36 -0400

    added dotenv and subbed values - tbd server.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,15 +1,21 @@
+//Used to create custom GraphQL errors
 const { GraphQLError } = require('graphql');
+//Used for creating and verifying tokens
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecretssshhhhhhh';
-const expiration = '2h';
+
+// Retrieve the secret and expiration values from environment variables
+const secret = process.env.JWT_SECRET;
+const expiration = process.env.JWT_EXPIRATION;
 
 module.exports = {
-  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+  // Define a custom GraphQL error for authentication failures
+  AuthenticationError: new GraphQLError("Could not authenticate user.", {
     extensions: {
-      code: 'UNAUTHENTICATED',
+      code: "UNAUTHENTICATED",
     },
   }),
+  // Function to sign a JWT token with the user's data
   signToken: function ({ email, name, _id }) {
     const payload = { email, name, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
