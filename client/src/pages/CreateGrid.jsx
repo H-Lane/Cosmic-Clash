f7a580f0a68a1794c39ad70188fca991b7895bb7
Hersey <helane1996@gmail.com>
2024-08-12 16:41:34 -0400

    Created basic page for grid creation and added createGrid mutation to front end mutation page

--- client/src/pages/CreateGrid.jsx
+++ client/src/pages/CreateGrid.jsx
@@ -1,4 +1,27 @@
 import React, { useState } from 'react';
 import EmptyGrid from '../components/EmptyGrid';
+import { useMutation } from '@apollo/client';
+import { createGrid } from '../utils/mutations'
 
-const gridCreation = 
+function generateGrid(props) {
+
+    const [shipState, setShipState] = useState({ shipName: '', position: '' });
+    const [createGrid] = useMutation(CREATE_GRID);
+
+    const handleShipSave = async (event) => {
+        event.preventDefault();
+        const mutationResponse = await createGrid({
+            variables: {
+                
+            }
+        });
+    };
+
+
+    return (
+        <div className="grid-container">
+            <EmptyGrid></EmptyGrid>
+            <button onClick={handleShipSave}>Save Ship Layout</button>
+        </div>
+    )
+}
