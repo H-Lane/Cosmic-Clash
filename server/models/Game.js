ac55ad679e300d75b88a2e40f6638ca6190ab302
Hersey <helane1996@gmail.com>
2024-08-14 18:39:27 -0400

    refined grid and game model

--- server/models/Game.js
+++ server/models/Game.js
@@ -1,53 +1,30 @@
 const { Schema, model } = require("mongoose");
 
-//Create a schema for the ships
-const shipSchema = new Schema({
-  shipName: {
-    type: String,
-    required: true,
-  },
-  position: [Number],
-});
-
-//Create the schema that accepts the Users ship grid
-const gridSchema = new Schema({
-  ships: [shipSchema],
-  userId: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
-  },
-  gameId: {
-    type: Schema.Types.ObjectId,
-    required: false,
-    ref: "Game",
-  },
-});
-
-//create the sub schema that will allow the userId to be saved along with fields for their hits and misses
-const playerSchema = new Schema({
-  userId: {
-    type: Schema.Types.ObjectId,
-  },
-  hits: [Number],
-  misses: [Number],
-});
-
 //This is the model containing our game. The first two keys are used to hold the information for our players. The turn key holds the userid of the player who is currently taking actions and sending requests, the attacks arrays store all of the attacks a player makes, and the grids arrays store the players ship information.
 const gameSchema = new Schema({
-  playerOne: [playerSchema],
-  playerTwo: [playerSchema],
+  playerOne: { 
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  playerTwo: Schema.Types.ObjectId,
   turn: {
     type: Schema.Types.ObjectId,
     required: true,
   },
+  //Utilize these attacks arrays to determine if a player has won and conditionally display where the user has been attacked
   firstAttacks: [Number],
   secondAttacks: [Number],
   winner: {
     type: Schema.Types.ObjectId,
   },
-  playerOneGrid: [gridSchema],
-  playerTwoGrid: [gridSchema],
+  playerOneGrid: {
+    type: Schema.Types.ObjectId,
+    ref: "Grid"
+  },
+  playerTwoGrid: {
+    type: Schema.Types.ObjectId,
+    ref: "Grid"
+  },
 });
 
 //When a user selects play, FIRST search for a game (FindOneAndUpdate) where secondUserId is Null and add that player to it, otherwise CREATE a game
