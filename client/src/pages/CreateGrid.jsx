74423483b5a5d3cc8e52e1b8639cf4f20ba9d1b5
Hersey <helane1996@gmail.com>
2024-08-14 04:31:04 -0400

    Added functionality to CreateGrid page to handle message modal creation and lock player from going over the number of spaces a ship can have

--- client/src/pages/CreateGrid.jsx
+++ client/src/pages/CreateGrid.jsx
@@ -11,6 +11,8 @@ function CreateGrid(props) {
   const [shipName, setShipName] = useState("");
   const [shipSize, setShipSize] = useState(0);
   const [spacesLeft, setSpacesLeft] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [message, setMessage] = useState("");
 
   //Here we set up a useState expecting an array of five objects
   const [ships, setShips] = useState([
@@ -41,26 +43,44 @@ function CreateGrid(props) {
     });
   };
 
-  //This is the listener that is updating the useState whenever a ship is placed
+  //This is the listener that is updating the useState for the ships whenever a ship is placed
   const handlePlacement = (e) => {
-    setShips(
-      ships.map((ship) => {
-        if (ship.shipName === shipName) {
-          ship.position.push(parseInt(e.target.dataset.position));
-          console.log(ship);
-        }
-        return ship;
-      })
-    );
-    setSpacesLeft(spacesLeft-1)
+    //first we check if there are still places left for the ship the user has selected
+    if (shipName && spacesLeft > 0) {
+      //call the ship setter function to map through our ships use state and check the ship selected against the names of the ships in the use state, then update the position array to contain the grid position of the clicked square.
+      setShips(
+        ships.map((ship) => {
+          if (ship.shipName === shipName) {
+            ship.position.push(parseInt(e.target.dataset.position));
+            console.log(ship);
+          }
+          return ship;
+        })
+      );
+      //Reduce the number of spaces left to select by one
+      setSpacesLeft(spacesLeft - 1);
+    } else if (!shipName) {
+      return;
+    } else {
+      //Set a message to display in the modal and call the modal to display
+      setMessage("Select Another Ship To Delpoy!");
+      setShowModal(true);
+    }
   };
 
   //Sets the selected ship from the ship component to the use states associated with them. Also displays the size of the ship to the page so that the User knows how many square they have left.
   //FUTURE: Change styling to display the selected ship and stop user from selecting another ship until the ship is fully placed
   const shipSetter = (e) => {
+    //We close the modal if necessary
+    closeModal();
     setShipName(e.target.dataset.name);
     setShipSize(parseInt(e.target.dataset.size));
     setSpacesLeft(parseInt(e.target.dataset.size));
+  };
+
+  //Method to handle the modal close button
+  const closeModal = () => {
+    setShowModal(false);
   };
 
   return (
@@ -69,6 +89,19 @@ function CreateGrid(props) {
       <button onClick={handleShipSave}>Save Ship Layout</button>
       <DisplayShips shipSetter={shipSetter} />
       <h1>SPACES LEFT: {spacesLeft}</h1>
+
+      {/* This is a modal to display any information stored in the message useState as a modal to a user. ORRNDREA WE WILL NEED THIS TO BE A STYLED MODAL */}
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            {/* This is the button to close the modal */}
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <h2>{message}</h2>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
