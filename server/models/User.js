b642eebf403e4486f815056753bab8c6817499c4
Hersey <helane1996@gmail.com>
2024-08-14 03:25:08 -0400

    Removed the HomeGrid component as it was extraneous

--- server/models/User.js
+++ server/models/User.js
@@ -37,42 +37,6 @@ userSchema.methods.validatePassword = async function(password) {
   return await bcrypt.compare(password, this.password);
 };
 
-// This is a pre-save hook that hashes the user's password so the raw password is never saved in our db
-// userSchema.pre("save", async function (next) {
-//   var user = this;
-
-//   // Only hash the password if the User has been modified (Or it is new)
-//   if (!user.isModified("password")) return next();
-
-//   try {
-//     // Generate a Salt for hashing
-//     const salt = await bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-//       if (err) return next(err);
-
-//       //hash the pw using the newly generated Salt
-//       bcrypt.hash(user.password, salt, function (err, hash) {
-//         if (err) return next(err);
-//         // override the cleartext password with the hashed one
-//         user.password = hash;
-//         next();
-//       });
-//     });
-//   } catch (err) {
-//     return next(err);
-//   }
-// });
-
-// Create a method the schema has access to that compares a given pw for login against the hashed pw in our db
-// userSchema.methods.validatePassword = async function (candidatePassword, cb) {
-//   bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
-//     if (err) return cb(err);
-//     cb(null, isMatch);
-//   });
-// };
-
-
-
-
 const User = model("User", userSchema);
 
 module.exports = User;
